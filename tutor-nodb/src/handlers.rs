e53508eaacd8f6a2073e43437779d58cef1858ed
pat <dimuziop@gmail.com>
2023-08-16 08:32:56 +0200

    feature: get courses tutor

--- tutor-nodb/src/handlers.rs
+++ tutor-nodb/src/handlers.rs
@@ -22,10 +22,17 @@ pub async fn new_course(new_course: web::Json<Course>, app_state: web::Data<AppS
         tutor_id: new_course.tutor_id,
         course_id: Some(course_id.clone()),
         course_name: new_course.course_name.clone(),
-        posted_time: Some(Utc::now().naive_utc())
+        posted_time: Some(Utc::now().naive_utc()),
     };
     app_state.courses.lock().unwrap().push(new_course);
     HttpResponse::Created().json(format!("Created course: {:?}", course_id))
+}
+
+pub async fn get_courses_for_tutor(app_state: web::Data<AppState>, params: web::Path<Uuid>) -> HttpResponse {
+    let tutor_id = params.into_inner();
+    let filter_courses = app_state.courses.lock().unwrap().clone().into_iter().filter(|course| course.tutor_id == tutor_id).collect::<Vec<Course>>();
+
+    HttpResponse::Ok().json(filter_courses)
 }
 
 #[cfg(test)]
@@ -33,14 +40,15 @@ mod test {
     use std::sync::Mutex;
     use actix_web::http::StatusCode;
     use super::*;
+    use std::str::FromStr;
 
     #[actix_rt::test]
     async fn post_course_test() {
-        let course = web::Json(Course{
+        let course = web::Json(Course {
             tutor_id: Uuid::new_v4(),
             course_name: "Some course name".into(),
             course_id: None,
-            posted_time: None
+            posted_time: None,
         });
 
         let app_state: web::Data<AppState> = web::Data::new(AppState {
@@ -51,5 +59,36 @@ mod test {
 
         let resp = new_course(course, app_state).await;
         assert_eq!(resp.status(), StatusCode::CREATED)
+    }
+
+    #[actix_rt::test]
+    async fn get_all_tutor_courses_empty() {
+        let app_state: web::Data<AppState> = web::Data::new(AppState {
+            health_check_response: "".to_string(),
+            visit_count: Mutex::new(0),
+            courses: Mutex::new(vec![]),
+        });
+
+        let tutor_id: web::Path<Uuid> = web::Path::from( Uuid::from_str("15d0830e-34d2-4914-873d-81725a5bc431").unwrap());
+        let resp = get_courses_for_tutor(app_state, tutor_id).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+
+    #[actix_rt::test]
+    async fn get_all_tutor_courses_with_data() {
+        let app_state: web::Data<AppState> = web::Data::new(AppState {
+            health_check_response: "".to_string(),
+            visit_count: Mutex::new(0),
+            courses: Mutex::new(vec![Course {
+                tutor_id: Uuid::from_str("15d0830e-34d2-4914-873d-81725a5bc431").unwrap(),
+                course_id: Some(Uuid::new_v4()),
+                course_name: "some name".to_string(),
+                posted_time: Some(Utc::now().naive_utc()),
+            }]),
+        });
+
+        let tutor_id: web::Path<Uuid> = web::Path::from( Uuid::from_str("15d0830e-34d2-4914-873d-81725a5bc431").unwrap());
+        let resp = get_courses_for_tutor(app_state, tutor_id).await;
+        assert_eq!(resp.status(), StatusCode::OK);
     }
 }

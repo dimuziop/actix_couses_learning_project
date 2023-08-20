be1c6ca71fb5e649806ca8fef333d3c621ff8d37
pat <dimuziop@gmail.com>
2023-08-20 14:04:21 +0200

    fix tests

--- tutor-db/src/iter4/handlers.rs
+++ tutor-db/src/iter4/handlers.rs
@@ -44,8 +44,11 @@ mod test {
     use std::env;
     use std::str::FromStr;
     use std::sync::Mutex;
+    use actix_web::body::MessageBody;
     use actix_web::http::StatusCode;
+    use actix_web::ResponseError;
     use dotenv::dotenv;
+    use log::debug;
     use sqlx::PgPool;
     use super::*;
 
@@ -67,13 +70,13 @@ mod test {
             course_name: "Some course name".into(),
         });
 
-        let resp = new_course(course, app_state).await;
+        let resp = new_course(course, app_state).await.unwrap();
         assert_eq!(resp.status(), StatusCode::CREATED);
     }
 
     #[actix_rt::test]
     async fn get_all_tutor_courses_empty() {
-        assert_eq!(true, true)
+//TODO
     }
 
     #[actix_rt::test]
@@ -90,13 +93,32 @@ mod test {
         });
 
         let tutor_id: web::Path<Uuid> = web::Path::from( Uuid::from_str("d709c2c9-eeb8-4b6b-a63d-25ef38c78e61").unwrap());
-        let resp = get_courses_for_tutor(app_state, tutor_id).await;
+        let resp = get_courses_for_tutor(app_state, tutor_id).await.unwrap();
         assert_eq!(resp.status(), StatusCode::OK);
     }
 
     #[actix_rt::test]
     async fn get_course_detail_not_found() {
-        assert_eq!(true, true)
+
+        dotenv().ok();
+
+        let database_url = env::var("DATABASE_URL").expect("Database url is not set");
+        let db_pool = PgPool::connect(&database_url).await.unwrap();
+
+        let app_state:  web::Data<AppState> = web::Data::new(AppState {
+            health_check_response: "".to_string(),
+            visit_count: Mutex::new(0),
+            db: db_pool
+        });
+
+        let params: web::Path<(Uuid, Uuid)> = web::Path::from( (Uuid::from_str("d709c2c9-eeb8-4b6b-a63d-25ef38c78e61").unwrap(), Uuid::from_str("70c57639-680a-44e8-a15b-e879d38aa854").unwrap()));
+        let resp = get_course_detail(app_state, params).await;
+        if resp.is_err() {
+            assert_eq!(resp.unwrap_err().status_code(), StatusCode::NOT_FOUND)
+        }
+        else {
+            assert_eq!(false, true)
+        }
     }
 
     #[actix_rt::test]
@@ -113,7 +135,7 @@ mod test {
         });
 
         let params: web::Path<(Uuid, Uuid)> = web::Path::from( (Uuid::from_str("d709c2c9-eeb8-4b6b-a63d-25ef38c78e61").unwrap(), Uuid::from_str("70c57639-680a-44e8-a15b-e879d38aa856").unwrap()));
-        let resp = get_course_detail(app_state, params).await;
+        let resp = get_course_detail(app_state, params).await.unwrap();
         assert_eq!(resp.status(), StatusCode::OK);
     }
 }
